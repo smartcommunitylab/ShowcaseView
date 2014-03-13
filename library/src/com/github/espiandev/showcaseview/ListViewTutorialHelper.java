0ab8ec2dcbe6263add343eb2677a047c82d4916c
chin8 <m.chini@trentorise.eu>
2014-03-13 11:33:38 +0100

    get correction by param

--- library/src/com/github/espiandev/showcaseview/ListViewTutorialHelper.java
+++ library/src/com/github/espiandev/showcaseview/ListViewTutorialHelper.java
@@ -26,24 +26,22 @@ import android.view.View;
 import android.widget.ListView;
 
 /**
- * Tutorial helper class that uses a list view as the list of reference
- * points for the tutorial steps.  
- * To manage tutorial it is necessary to
- * provide the parent activity where the tutorial is shown 
- * and the {@link TutorialProvider} instance to provide
- * the information about the tutorial items and views, as 
+ * Tutorial helper class that uses a list view as the list of reference points
+ * for the tutorial steps. To manage tutorial it is necessary to provide the
+ * parent activity where the tutorial is shown and the {@link TutorialProvider}
+ * instance to provide the information about the tutorial items and views, as
  * well as to handle the tutorial termination.
+ * 
  * @author raman
- *
+ * 
  */
 public class ListViewTutorialHelper extends TutorialHelper {
-
 
 	private static final String TUT_PREFS = "tut_prefs";
 	private static final String TOUR_PREFS = "wantTour";
 
 	private int lastShown = -1;
-	
+
 	/**
 	 * @param mActivity
 	 * @param mItemList
@@ -59,18 +57,22 @@ public class ListViewTutorialHelper extends TutorialHelper {
 	}
 
 	/**
-	 * Fill in the parameters for the tutorial items using 
-	 * the element at 'index' in the view to calculate the position and width.
-	 * If needed, the required element will be scrolled to.
+	 * Fill in the parameters for the tutorial items using the element at
+	 * 'index' in the view to calculate the position and width. If needed, the
+	 * required element will be scrolled to.
 	 * 
-	 * @param item {@link TutorialItem} instance to populate
-	 * @param index position of the element in the list 
-	 * @param listView {@link ListView} holding the steps of the tutorial
-	 * @param itemId layout ID of the view to search for
+	 * @param item
+	 *            {@link TutorialItem} instance to populate
+	 * @param index
+	 *            position of the element in the list
+	 * @param listView
+	 *            {@link ListView} holding the steps of the tutorial
+	 * @param itemId
+	 *            layout ID of the view to search for
 	 */
 	public static void fillTutorialItemParams(TutorialItem item, int index, ListView listView, int itemId) {
 		if (listView != null) {
-		
+
 			int firstVisible = listView.getFirstVisiblePosition();
 			int lastVisible = listView.getLastVisiblePosition();
 			int page = lastVisible - firstVisible;
@@ -81,66 +83,108 @@ public class ListViewTutorialHelper extends TutorialHelper {
 				shift = -v.getTop();
 				if (v != null && v.findViewById(itemId) != null)
 
-				listView.setSelection(index);
-				
-				
-			}else if (index >= lastVisible && listView.getChildAt(index - firstVisible) != null) {
+					listView.setSelection(index);
+
+			} else if (index >= lastVisible && listView.getChildAt(index - firstVisible) != null
+					&& !(index == listView.getAdapter().getCount() - 1)) {
 				v = listView.getChildAt(index - firstVisible);
 				if (listView.getAdapter().getCount() - lastVisible <= page) {
 					// last items < one page
-					v = listView.getChildAt(lastVisible-firstVisible-2);		
+					v = listView.getChildAt(lastVisible - firstVisible - 2);
 					if (v != null && v.findViewById(itemId) != null)
-						listView.setSelection(firstVisible+2);
+						listView.setSelection(firstVisible + 2);
 				} else {
 					shift = -v.getTop();
 					if (v != null && v.findViewById(itemId) != null)
 						listView.setSelection(index);
-					
+
 				}
 
-			}else {
+			} else {
 				v = listView.getChildAt(index - firstVisible);
 			}
-			
+
 			if (v != null) {
 				View logo = v.findViewById(itemId);
 				if (logo != null) {
 					item.width = logo.getWidth();
 					item.position = new int[2];
 					logo.getLocationOnScreen(item.position);
-					item.position[1] += shift;
+					item.position[1] += shift - 12; // correction
 				}
 			}
 		}
 	}
-	
+
+
+	public static void fillTutorialItemParamsWithCorrection(TutorialItem item, int index, ListView listView, int itemId, int xCorrection, int yCorrection) {
+		if (listView != null) {
+
+			int firstVisible = listView.getFirstVisiblePosition();
+			int lastVisible = listView.getLastVisiblePosition();
+			int page = lastVisible - firstVisible;
+			int shift = 0;
+			View v = null;
+			if (index <= firstVisible) {
+				v = listView.getChildAt(index);
+				shift = -v.getTop();
+				if (v != null && v.findViewById(itemId) != null)
+
+					listView.setSelection(index);
+
+			} else if (index >= lastVisible && listView.getChildAt(index - firstVisible) != null
+					&& !(index == listView.getAdapter().getCount() - 1)) {
+				v = listView.getChildAt(index - firstVisible);
+				if (listView.getAdapter().getCount() - lastVisible <= page) {
+					// last items < one page
+					v = listView.getChildAt(lastVisible - firstVisible - 2);
+					if (v != null && v.findViewById(itemId) != null)
+						listView.setSelection(firstVisible + 2);
+				} else {
+					shift = -v.getTop();
+					if (v != null && v.findViewById(itemId) != null)
+						listView.setSelection(index);
+
+				}
+
+			} else {
+				v = listView.getChildAt(index - firstVisible);
+			}
+
+			if (v != null) {
+				View logo = v.findViewById(itemId);
+				if (logo != null) {
+					item.width = logo.getWidth();
+					item.position = new int[2];
+					logo.getLocationOnScreen(item.position);
+					item.position[0] += xCorrection;
+					item.position[1] += shift +yCorrection; // correction
+				}
+			}
+		}
+	}
+
 	private void startShow() {
 		int idx = getLastTutorialShown() + 1;
 		if (idx >= 0 && idx < mProvider.size()) {
 			lastShown = idx;
-			displayShowcaseView(mProvider.getItemAt(lastShown), mProvider.size()-1==lastShown);
+			displayShowcaseView(mProvider.getItemAt(lastShown), mProvider.size() - 1 == lastShown);
 		} else
 			setWantTour(mActivity, false);
-		}
+	}
+
 	private boolean wantTour() {
 		return getTutorialPreferences(mActivity).getBoolean(TOUR_PREFS, false);
 	}
-	
+
 	private void reset() {
 		setLastShown(-1);
 	}
 
 	private void displayShowcaseView(TutorialItem item, boolean isLast) {
-		BaseTutorialActivity.newIstance(
-				mActivity, 
-				item.position,
-				item.width,
-				Color.WHITE, 
-				null,
+		BaseTutorialActivity.newIstance(mActivity, item.position, item.width, Color.WHITE, null,
 				item.title == null ? mActivity.getString(item.titleId) : item.title,
-				item.msg == null ? mActivity.getString(item.msgId) : item.msg,
-				isLast,
-				TUTORIAL_REQUEST_CODE,
+				item.msg == null ? mActivity.getString(item.msgId) : item.msg, isLast, TUTORIAL_REQUEST_CODE,
 				TutorialActivity.class);
 	}
 
@@ -165,14 +209,13 @@ public class ListViewTutorialHelper extends TutorialHelper {
 		edit.commit();
 	}
 
-
 	public void onTutorialActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == TUTORIAL_REQUEST_CODE) {
 			if (resultCode == Activity.RESULT_OK) {
 				String resData = data.getExtras().getString(BaseTutorialActivity.RESULT_DATA);
 				if (resData.equals(BaseTutorialActivity.OK)) {
 					setLastShown(lastShown);
-					if (lastShown == mProvider.size()-1) {
+					if (lastShown == mProvider.size() - 1) {
 						mProvider.onTutorialFinished();
 					} else if (wantTour()) {
 						startShow();
